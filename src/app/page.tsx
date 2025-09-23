f3c8113a1ffba9a6e68ad992b86ebe16782e81f5
Gabriel Augusto <gabrielaugustoassisnascimento@gmail.com>
2025-09-22 22:07:59 -0300

    Versão 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+// src/app/page.tsx
 import Header from "@/components/Header";
 import Skills from "@/components/Skills";
 import { Particles } from "@/components/ui/particles";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center">
-      <Header />
-      <Skills />
+    <div className="main-page-container">
+      <div className="flex flex-col items-center">
+        <Header />
+        <Skills />
+      </div>
       <div className='particles-grid-container'>
         <Particles
           quantity={200}
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
+// src/components/NavBar/index.tsx
+"use client"
+import React, { useState } from 'react'
 import './styles.css'
 import { ShimmerButton } from "@/components/ui/shimmer-button";
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-logo"><img src="/logo.png" alt="logo" className='w-20 h-20' /></div>
-        <ul className="navbar-links">
-          <li><a href="/">Início</a></li>
-          <li><a href="/about">Sobre</a></li>
-          <li><a href="/projects">Projetos</a></li>
-          <li><a href="/login">Login</a></li>
+        
+        {/* Botão Hambúrguer para mobile */}
+        <button className="hamburger-menu" onClick={toggleMenu} aria-label="Toggle navigation menu">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </button>
+
+        <ul className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
+          <li><a href="/" onClick={toggleMenu}>Início</a></li>
+          <li><a href="/about" onClick={toggleMenu}>Sobre</a></li>
+          <li><a href="/projects" onClick={toggleMenu}>Projetos</a></li>
+          <li><a href="/login" onClick={toggleMenu}>Login</a></li>
 
           <li><a href="/contact">
-            <ShimmerButton className="shadow-2xl">
+            <ShimmerButton className="shadow-2xl" onClick={toggleMenu}>
               <span >
                 Contato
               </span>
